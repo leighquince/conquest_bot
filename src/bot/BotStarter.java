f4bb92dc90f01cff613dec5c92f63d4c86a8d5d2
Leigh Quince <leighquince@quincewebdesign.com>
2013-12-04 22:26:24 +0100

    added attack logic

--- src/bot/BotStarter.java
+++ src/bot/BotStarter.java
@@ -97,7 +97,6 @@ public class BotStarter implements Bot
 		
 		ArrayList<PlaceArmiesMove> placeArmiesMoves = new ArrayList<PlaceArmiesMove>();
 		String myName = state.getMyPlayerName();
-		int armies = 2;
 		int armiesLeft = state.getStartingArmies();
 		LinkedList<Region> visibleRegions = state.getVisibleMap().getRegions();
 		LinkedList<Region> myRegions = new LinkedList<Region>();
@@ -117,109 +116,231 @@ public class BotStarter implements Bot
 		
 		int armiesPlacing = 1;
 		for (Region region : myRegions) {
-			if(armiesLeft > 0)
+			if(!alreadyPlaced(placeArmiesMoves,region) && armiesLeft > 0)
 			{	
 				//region i own majority of where enemy has invaded
 				if(region.getSuperRegion().ownedByPlayer() != myName)
 				{
-					if(region.getSuperRegion().getPercentageOwned(myName)>=0.75 && !superRegionsAdded.contains(region.getSuperRegion()))
-					{
-						superRegionsAdded.add(region.getSuperRegion());
-						armiesPlacing = armiesLeft == 1? 1: (int)(armiesLeft*0.7);
-						placeArmiesMoves.add(new PlaceArmiesMove(myName, region, armiesPlacing));
-						armiesLeft = armiesPlacing;
-					}
-					//border with heavy enemy
-				}else
-				if(region.getSuperRegion().ownedByPlayer() != myName)
-				{
-					if(region.getSuperRegion().getPercentageOwned(myName)>=0.5 && !superRegionsAdded.contains(region.getSuperRegion()))
-					{
 						
-						superRegionsAdded.add(region.getSuperRegion());
-						armiesPlacing = armiesLeft == 1? 1: (int)(armiesLeft*0.5);
-						placeArmiesMoves.add(new PlaceArmiesMove(myName, region, armiesPlacing));
-						armiesLeft = armiesPlacing;
-					}
-					//border with heavy enemy
-				}else if(region.getSuperRegion().ownedByPlayer() == myName)
-				{
-					for (Region neighbour : region.getNeighbors()) {
-						if(armiesLeft > 0)
+					if(region.getSuperRegion().getPercentageOwned(myName)>=0.75)
+					{
+						if(!this.surrondedByFriends(region, state))
 						{
-							if((neighbour.getSuperRegion() != region.getSuperRegion()) && !neighbour.ownedByPlayer(myName))
-							{
-								if(neighbour.getArmies()>= region.getArmies())
-								{
-									armiesPlacing = armiesLeft == 1? 1: (int)(armiesLeft*0.5);
-									armiesPlacing = armiesPlacing+region.getArmies()>=neighbour.getArmies()?armiesPlacing:armiesLeft;
-									placeArmiesMoves.add(new PlaceArmiesMove(myName, region, armiesPlacing));
-									armiesLeft = armiesPlacing;
-								}else{
-									if(neighbour.getArmies()<(int)region.getArmies()*0.9)
-									{
-										armiesPlacing = armiesLeft == 1? 1: (int)(armiesLeft*0.2);
-										placeArmiesMoves.add(new PlaceArmiesMove(myName, region, armiesPlacing));
-										armiesLeft = armiesPlacing;
-									}
-								}
-							}
-						}else{
-							break;
+							armiesPlacing = armiesLeft <2 ? 1: (int)(armiesLeft*0.7);
+							placeArmiesMoves.add(new PlaceArmiesMove(myName, region, armiesPlacing));
+							armiesLeft = armiesPlacing;
+					
 						}
 					}
-				}else if(enemyIsWeak(region, state)){
-					
-					armiesPlacing = armiesLeft == 1? 1: (int)(armiesLeft*0.3);
-					placeArmiesMoves.add(new PlaceArmiesMove(myName, region, armiesPlacing));
-					armiesLeft = armiesPlacing;
-					
+					//border with heavy enemy
 				}
-				
-			}else{
-				break;
-			}
-		}		
-		//priority
-		 //First move gain a super region of size 4!
-		 //defend super region borders that neighbour enemy (always try to have a matching number of defenders)
-		 //gain regions in super regions where i have a presence
-		  //Priorities this with the weighting of how many men i have in the super region vs the number the enemy has
-		
-		//get a super region
-//		for (Region region : visibleRegions) {
-//			if(armiesLeft > 0)
-//			{
-//				if(region.getSuperRegion().getId() == 6 || region.getSuperRegion().getId() == 2 )
-//				{
-//					if(region.getSuperRegion().ownedByPlayer() != myName)
-//					{
-//						if(region.ownedByPlayer(myName))
-//						{
-//							placeArmiesMoves.add(new PlaceArmiesMove(myName, region, armies2));
-//							armiesLeft = armies2;
-//						}	
-//					}
-//					
-//				}
-//			}
-//		}
-		 
-		
-		while(armiesLeft > 0)
-		{
-			double rand = Math.random();
-			int r = (int) (rand*visibleRegions.size());
-			Region region = visibleRegions.get(r);
-			
-			if(region.ownedByPlayer(myName))
-			{
-				placeArmiesMoves.add(new PlaceArmiesMove(myName, region, armies));
-				armiesLeft -= armies;
 			}
 		}
 		
+		for (Region region : myRegions) {
+			if(alreadyPlaced(placeArmiesMoves,region) || !(armiesLeft >0))
+			{
+				break;
+			}
+				
+			if(region.getSuperRegion().ownedByPlayer() != myName)
+			{
+				if(region.getSuperRegion().getPercentageOwned(myName)>=0.5 && !superRegionsAdded.contains(region.getSuperRegion()))
+				{
+					
+					superRegionsAdded.add(region.getSuperRegion());
+					armiesPlacing = armiesLeft == 1? 1: (int)(armiesLeft*0.5);
+					placeArmiesMoves.add(new PlaceArmiesMove(myName, region, armiesPlacing));
+					armiesLeft = armiesPlacing;
+				}
+				
+			}
+		}
+		
+		
+		//border with heavy enemy
+		for (Region region : myRegions) {
+			if(alreadyPlaced(placeArmiesMoves,region) || !(armiesLeft >0))
+			{
+				break;
+			}
+			if(region.getSuperRegion().ownedByPlayer() == myName)
+			{
+				for (Region neighbour : region.getNeighbors()) {
+					if(armiesLeft > 0)
+					{
+						if((neighbour.getSuperRegion() != region.getSuperRegion()) && !neighbour.ownedByPlayer(myName))
+						{
+							if(neighbour.getArmies()>= region.getArmies())
+							{
+								armiesPlacing = armiesLeft == 1? 1: (int)(armiesLeft*0.5);
+								armiesPlacing = armiesPlacing+region.getArmies()>=neighbour.getArmies()?armiesPlacing:armiesLeft;
+								placeArmiesMoves.add(new PlaceArmiesMove(myName, region, armiesPlacing));
+								armiesLeft = armiesPlacing;
+							}else{
+								if(neighbour.getArmies()<(int)region.getArmies()*0.9)
+								{
+									armiesPlacing = armiesLeft == 1? 1: (int)(armiesLeft*0.2);
+									placeArmiesMoves.add(new PlaceArmiesMove(myName, region, armiesPlacing));
+									armiesLeft = armiesPlacing;
+								}
+							}
+						}
+					}else{
+						break;
+					}
+				}
+			}
+		}
+		for (Region region : myRegions) {
+			if(alreadyPlaced(placeArmiesMoves,region) || !(armiesLeft >0))
+			{
+				break;
+			}
+			
+		 if(enemyIsWeak(region, state)){
+				
+				armiesPlacing = armiesLeft == 1? 1: (int)(armiesLeft*0.3);
+				placeArmiesMoves.add(new PlaceArmiesMove(myName, region, armiesPlacing));
+				armiesLeft = armiesPlacing;
+				
+			}
+			
+		}
+		
+		if(armiesLeft >0)
+		{
+			Region region = myRegions.get((int)(Math.random()*myRegions.size()));
+			placeArmiesMoves.add(new PlaceArmiesMove(myName, region, armiesLeft));
+		}
 		return placeArmiesMoves;
+	}
+
+	
+	@Override
+	/**
+	 * This method is called for at the second part of each round. This example attacks if a region has
+	 * more than 6 armies on it, and transfers if it has less than 6 and a neighboring owned region.
+	 * @return The list of PlaceArmiesMoves for one round
+	 */
+	public ArrayList<AttackTransferMove> getAttackTransferMoves(BotState state, Long timeOut) 
+	{
+		
+		ArrayList<AttackTransferMove> attackTransferMoves = new ArrayList<AttackTransferMove>();
+		ArrayList<PlaceArmiesMove> placeArmiesMoves = new ArrayList<PlaceArmiesMove>();
+		String myName = state.getMyPlayerName();
+		
+		LinkedList<Region> visibleRegions = state.getVisibleMap().getRegions();
+		LinkedList<Region> myRegions = new LinkedList<Region>();
+		LinkedList<Region> enemyRegions = new LinkedList<Region>();
+		
+		
+		
+		//get my regions
+		for (Region region : visibleRegions) {
+			if(region.ownedByPlayer(myName))
+			{
+				myRegions.add(region);
+			}else{
+				enemyRegions.add(region);
+			}
+		}
+		
+		
+		for (Region fromRegion : myRegions) {	
+			if(surrondedByFriends(fromRegion,state))
+			{
+				if( mostInNeedNeighbour(fromRegion.getNeighbors(),state,fromRegion) != null)
+				{	
+					Region toRegion = mostInNeedNeighbour(fromRegion.getNeighbors(),state,fromRegion);
+					if(fromRegion.getArmies() >2)
+					{
+						int armiesToMove = fromRegion.getArmies()-2;
+						attackTransferMoves.add(new AttackTransferMove(myName, fromRegion, toRegion, armiesToMove));
+					}
+				}
+			}
+				
+		}
+		
+		//don't attack from the paces i have moved from
+		for (AttackTransferMove movesSoFar : attackTransferMoves) {		
+			myRegions.remove(movesSoFar.getFromRegion());
+		}
+		
+		for (Region fromRegion : myRegions) {
+			if(fromRegion.getSuperRegion().getPercentageOwned(myName)>=0.5)
+			{
+				if(!surrondedByFriends(fromRegion,state))
+				{
+					for (Region toRegion : fromRegion.getNeighbors()) {
+						if(!toRegion.ownedByPlayer(state.getMyPlayerName()))
+						{
+							if(toRegion.getArmies() < fromRegion.getArmies()*0.6)
+							{
+								
+								int armiesToMove = (int) (toRegion.getArmies()+(toRegion.getArmies()*0.6));
+								if(armiesToMove < 4)
+								{
+									armiesToMove = 4;
+								}
+								if(armiesToMove >= fromRegion.getArmies())
+								{
+									continue;
+								}
+																
+								attackTransferMoves.add(new AttackTransferMove(myName, fromRegion, toRegion, armiesToMove));
+								if((fromRegion.getArmies()-armiesToMove > 5))
+								{
+									continue;
+								}else{
+									break;
+								}
+							}
+						}
+					}	
+				}
+			}		
+		}
+		
+		//don't attack from the paces i have moved from
+		for (AttackTransferMove movesSoFar : attackTransferMoves) {		
+			myRegions.remove(movesSoFar.getFromRegion());
+		}
+		
+		//attack weak regions
+		for (Region fromRegion1 : myRegions) {
+			for (Region toRegion : fromRegion1.getNeighbors()) {
+				if(!toRegion.ownedByPlayer(state.getMyPlayerName()))
+				{
+					if(toRegion.getArmies()-(fromRegion1.getArmies()*0.9)<0)
+					{
+						if(fromRegion1.getArmies() >2)
+						{
+							int armiesToMove = fromRegion1.getArmies()-2;
+							//System.err.println(String.format("Attacking Region %s from %s with %s %n",toRegion.getId(),fromRegion.getId(),armiesToMove));
+							attackTransferMoves.add(new AttackTransferMove(myName, fromRegion1, toRegion, armiesToMove));
+							break;
+						}
+					}
+				}
+				
+			}
+			
+		}		
+		return attackTransferMoves;
+	}
+	
+	private boolean alreadyPlaced(ArrayList<PlaceArmiesMove> placeArmiesMoves,
+			Region region) {
+		for ( PlaceArmiesMove placeArmiesMove : placeArmiesMoves) {
+			
+			if(placeArmiesMove.getRegion().equals(region))
+			{
+				return true;
+			}
+		}
+		return false;
 	}
 
 	private boolean enemyIsWeak(Region region, BotState state) {
@@ -229,7 +350,7 @@ public class BotStarter implements Bot
 		
 					if(!neighbour.ownedByPlayer(state.getMyPlayerName()))
 					{
-						if(neighbour.getArmies()<(int)region.getArmies()*0.7)
+						if(neighbour.getArmies()<(int)region.getArmies()*0.8)
 						{
 								return true;
 							
@@ -271,13 +392,13 @@ public class BotStarter implements Bot
 	private Region mostInNeedNeighbour(LinkedList<Region> neighbours, BotState state, Region region)
 	{
 		Map<Region,Integer> mostIneed = new HashMap<Region,Integer>();
-		for (Region regionNeighbour : neighbours) {
-			if(!regionNeighbour.ownedByPlayer(state.getMyPlayerName()))
+		for (Region neighboursNeighbours : neighbours) {
+			if(neighboursNeighbours.ownedByPlayer(state.getMyPlayerName()))
 			{
-				for (Region  neighbour: regionNeighbour.getNeighbors()) {
-					if(!regionNeighbour.ownedByPlayer(state.getMyPlayerName()))
+				for (Region  neighbour: neighboursNeighbours.getNeighbors()) {
+					if(!neighbour.ownedByPlayer(state.getMyPlayerName()))
 					{
-						mostIneed.put(neighbour, new Integer((int) (neighbour.getArmies()-(regionNeighbour.getArmies()*0.9))));
+						mostIneed.put(neighboursNeighbours, new Integer((int) (neighbour.getArmies()-(neighboursNeighbours.getArmies()*0.9))));
 					}
 				}	
 			}
@@ -300,113 +421,7 @@ public class BotStarter implements Bot
 			}
 		}
 			
-		return rank <= 0?null:regionRank;
-	}
-	@Override
-	/**
-	 * This method is called for at the second part of each round. This example attacks if a region has
-	 * more than 6 armies on it, and transfers if it has less than 6 and a neighboring owned region.
-	 * @return The list of PlaceArmiesMoves for one round
-	 */
-	public ArrayList<AttackTransferMove> getAttackTransferMoves(BotState state, Long timeOut) 
-	{
-		
-		ArrayList<AttackTransferMove> attackTransferMoves = new ArrayList<AttackTransferMove>();
-		ArrayList<PlaceArmiesMove> placeArmiesMoves = new ArrayList<PlaceArmiesMove>();
-		String myName = state.getMyPlayerName();
-		int armies = 2;
-		int armiesLeft = state.getStartingArmies();
-		LinkedList<Region> visibleRegions = state.getVisibleMap().getRegions();
-		LinkedList<Region> myRegions = new LinkedList<Region>();
-		LinkedList<Region> enemyRegions = new LinkedList<Region>();
-		LinkedList<SuperRegion> superRegionsAdded = new LinkedList<SuperRegion>();
-		
-		
-		//get my regions
-		for (Region region : visibleRegions) {
-			if(region.ownedByPlayer(myName))
-			{
-				myRegions.add(region);
-			}else{
-				enemyRegions.add(region);
-			}
-		}
-		
-		
-		for (Region fromRegion : myRegions) {
-	
-				//regions i don't need to defend move to a neighbout in need
-				if(fromRegion.getSuperRegion().ownedByPlayer() == myName )
-				{
-					if(surrondedByFriends(fromRegion,state))
-					{
-						if( mostInNeedNeighbour(fromRegion.getNeighbors(),state,fromRegion) != null)
-						{	
-							Region toRegion = mostInNeedNeighbour(fromRegion.getNeighbors(),state,fromRegion);
-							int armiesToMove = fromRegion.getArmies()-2;
-							armiesToMove = fromRegion.getArmies()-armiesToMove>=2?armiesToMove:0;
-							if(armiesToMove>0)
-							{
-								attackTransferMoves.add(new AttackTransferMove(myName, fromRegion, toRegion, armiesToMove));
-						
-							}
-						}
-					}
-				}
-		}
-		
-		//don't attack from the paces i have moved from
-		for (AttackTransferMove movesSoFar : attackTransferMoves) {		
-			myRegions.remove(movesSoFar.getFromRegion());
-		}
-		
-		//attack weak regions
-		for (Region fromRegion : myRegions) {
-			for (Region toRegion : fromRegion.getNeighbors()) {
-				if(toRegion.getArmies()-(fromRegion.getArmies()*0.9)>0)
-				{
-					int armiesToMove = fromRegion.getArmies()-2;
-					armiesToMove = armiesToMove>=2?armiesToMove:0;
-					System.err.println(String.format("Attaching Region %s from %s with %s %n",toRegion.getId(),fromRegion.getId(),armiesToMove));
-					attackTransferMoves.add(new AttackTransferMove(myName, fromRegion, toRegion, armiesToMove));
-					break;
-				}
-				
-			}
-			
-		}
-		
-		
-//		for(Region fromRegion : state.getVisibleMap().getRegions())
-//		{
-//			if(fromRegion.ownedByPlayer(myName)) //do an attack
-//			{
-//				ArrayList<Region> possibleToRegions = new ArrayList<Region>();
-//				possibleToRegions.addAll(fromRegion.getNeighbors());
-//				
-//				while(!possibleToRegions.isEmpty())
-//				{
-//					double rand = Math.random();
-//					int r = (int) (rand*possibleToRegions.size());
-//					Region toRegion = possibleToRegions.get(r);
-//					
-//					if(!toRegion.getPlayerName().equals(myName) && fromRegion.getArmies() > toRegion.getArmies()+(toRegion.getArmies()/7)) //do an attack
-//					{
-//						attackTransferMoves.add(new AttackTransferMove(myName, fromRegion, toRegion, armies));
-//						break;
-//					}
-//					else if(toRegion.getPlayerName().equals(myName) && (toRegion.getSuperRegion().getId() != fromRegion.getSuperRegion().getId()) && fromRegion.getArmies() > 2) //do a transfer
-//					{
-//						attackTransferMoves.add(new AttackTransferMove(myName, fromRegion, toRegion, armies));
-//						break;
-//					}
-//					else
-//						possibleToRegions.remove(toRegion);
-//				}
-//			}
-//		}
-		
-		return attackTransferMoves;
+		return rank == null || rank <= 0?null:regionRank;
 	}
 
 	public static void main(String[] args)
